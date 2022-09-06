be7c5007d321990a7931d9beb8ee564fdd40e2ef
CiriloSilva <liro.slva@hotmail.com>
2022-09-06 10:25:54 -0300

    Correção da cor do Search

--- resources/views/funcionarios/busca.blade.php
+++ resources/views/funcionarios/busca.blade.php
@@ -11,13 +11,13 @@
 			<div class="col-sm-4">
 				<div class="container">
 					<form class="d-flex flex-row mt-4" role="search" type="get" action="{{ route('funcionarios-busca') }}">
-						<a href="{{ route('funcionarios-create') }}" class="btn btn-success me-2">
+						<a href="{{ route('funcionarios-create') }}" class="btn btn-primary me-2">
 							<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
 								<path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"/>
 							</svg>
 						</a>
 				        <input class="form-control me-2 bg" name="consulta" type="search" placeholder="Procurar funcionário" aria-label="Procurar">
-				        <button class="btn btn-dark" type="submit">
+				        <button class="btn btn-warning" type="submit">
 							<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
 								<path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
 							</svg>
@@ -61,12 +61,6 @@
 							<path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
 						</svg>
 			      	</a>
-			      	<!-- <a href="#" class="btn btn-primary me-2" onclick="showModal()">
-						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-							<path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-							<path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-						</svg>
-			      	</a> -->
 		      		<form action="{{ route('funcionarios-destroy', ['id'=>$funcionario->id]) }}" method="POST">
 						@csrf
 						@method('delete')
@@ -82,29 +76,5 @@
 		    @endforeach
 		  </tbody>
 		</table>
-		<!-- <div class="modal" id="idModal" tabindex="-1">
-			<div class="modal-dialog">
-				<div class="modal-content">
-					<div class="modal-header">
-						<h5 class="modal-title">Editar Funcionário</h5>
-						<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-					</div>
-					<div class="modal-body">
-						Corpo
-					</div>
-					<div class="modal-footer">
-						<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-						<button type="button" class="btn btn-primary">Save changes</button>
-					</div>
-				</div>
-			</div>
-		</div>
-		<script>
-			function showModal(){
-				let modal = document.getElementById('idModal');
-				let mostraModal = new bootstrap.Modal(modal);
-				mostraModal.show();
-			}
-		</script> -->
 	</div>
 @endsection
--- resources/views/funcionarios/index.blade.php
+++ resources/views/funcionarios/index.blade.php
@@ -61,12 +61,6 @@
 							<path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
 						</svg>
 			      	</a>
-			      	<!-- <a href="#" class="btn btn-primary me-2" onclick="showModal()">
-						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-							<path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-							<path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-						</svg>
-			      	</a> -->
 		      		<form action="{{ route('funcionarios-destroy', ['id'=>$funcionario->id]) }}" method="POST">
 						@csrf
 						@method('delete')
@@ -82,29 +76,5 @@
 		    @endforeach
 		  </tbody>
 		</table>
-		<!-- <div class="modal" id="idModal" tabindex="-1">
-			<div class="modal-dialog">
-				<div class="modal-content">
-					<div class="modal-header">
-						<h5 class="modal-title">Editar Funcionário</h5>
-						<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-					</div>
-					<div class="modal-body">
-						Corpo
-					</div>
-					<div class="modal-footer">
-						<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-						<button type="button" class="btn btn-primary">Save changes</button>
-					</div>
-				</div>
-			</div>
-		</div>
-		<script>
-			function showModal(){
-				let modal = document.getElementById('idModal');
-				let mostraModal = new bootstrap.Modal(modal);
-				mostraModal.show();
-			}
-		</script> -->
 	</div>
 @endsection
